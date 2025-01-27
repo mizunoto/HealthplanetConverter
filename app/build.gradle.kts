152c76268e149e8a361ad39b55ca071d1370a608
mizunoto <36124523+mizunoto@users.noreply.github.com>
2025-01-28 03:50:37 +0900

    ver0.1.4 α1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 34
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.1"
+        versionName = "0.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
